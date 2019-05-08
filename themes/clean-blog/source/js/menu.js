035d6cf525c54509571878376a081450790a79a0
JOÃO VITOR DA COSTA ANDRADE <jvcostaandrade7399@gmail.com>
2019-05-08 18:03:50 -0300

    Novo design com UIkit

--- themes/clean-blog/source/js/menu.js
+++ themes/clean-blog/source/js/menu.js
@@ -38,4 +38,13 @@
       });
   }
 
+  $(document).on('click','.navbar-collapse.in',function(e) {
+
+      if( $(e.target).is('a') && ( $(e.target).attr('class') != 'dropdown-toggle' ) ) {
+          $(this).collapse('hide');
+      }
+
+  });
+
+
 })(jQuery); // End of use strict
